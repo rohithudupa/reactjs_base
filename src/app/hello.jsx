62001e7d87eb4f62db4aaae3a133cf228cf60da6
rohithudupa <rohithudupar@gmail.com>
2017-10-19 13:15:53 +0530

    react_base

---
+++ src/app/hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+class Hello extends React.Component {
+  render() {
+    return <p>Hello world!!</p>
+  }
+}
+ReactDOM.render(<Hello/>, document.getElementById('hello'));
